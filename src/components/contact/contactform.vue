0b5bc65b8deac7d673a5b0b9ee3125984349c890
Miracle Banks <miraclebanks@Miracles-Work-Laptop.local>
2024-04-14 14:47:37 -0700

    Making page responsive

--- src/components/contact/contactform.vue
+++ src/components/contact/contactform.vue
@@ -41,7 +41,7 @@
 <template>
   <div class="contact-container">
     <div class="form-container">
-      <h2>Contact Us</h2>
+      <h1>Contact Us</h1>
       <form @submit.prevent="submitForm">
         <div>
           <label for="name">Name:</label>
@@ -63,51 +63,87 @@
         <button type="submit">Submit</button>
       </form>
     </div>
+    <div class="contact-container-image">
+      <img src="../../assets/find-help.png" />
+    </div>
   </div>
-  <div></div>
 </template>
 <style lang="scss">
   /* Add component-specific styles here */
   .contact-container {
     display: flex;
-    align-items: center;
-  }
+    flex-direction: row;
 
-  .form-container {
-    div {
+    &-image {
+      margin: 16px;
+      min-width: 500px;
+      border-radius: 4px;
+      box-sizing: border-box;
+
+      img {
+        max-width: 100%;
+        height: auto;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
+      }
+    }
+
+    .form-container {
       display: flex;
       flex-direction: column;
+      justify-content: space-evenly;
+      box-sizing: border-box;
+      margin: 16px;
+      flex: 1;
+      min-width: 500px;
+      width: 100%;
+      height: auto;
+      border: 4px solid black;
+      border-radius: 8px;
+      padding: 40px 40px;
+      div {
+        display: flex;
+        flex-direction: column;
+      }
+      form {
+        display: flex;
+        flex-direction: column;
+      }
+
+      label {
+        margin-bottom: 0.5rem;
+      }
+
+      input,
+      textarea {
+        padding: 0.5rem;
+        margin-bottom: 1rem;
+      }
+
+      button {
+        padding: 0.5rem 1rem;
+        background-color: #007bff;
+        color: #fff;
+        border: 1px solid white;
+        border-radius: 8px;
+        cursor: pointer;
+      }
+
+      button:hover {
+        background-color: #0056b3;
+      }
     }
-    min-width: 500px;
-    border: 4px solid black;
-    border-radius: 10%;
-    padding: 40px 40px;
   }
-
-  form {
-    display: flex;
-    flex-direction: column;
-  }
-
-  label {
-    margin-bottom: 0.5rem;
-  }
-
-  input,
-  textarea {
-    padding: 0.5rem;
-    margin-bottom: 1rem;
-  }
-
-  button {
-    padding: 0.5rem 1rem;
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-  }
-
-  button:hover {
-    background-color: #0056b3;
+  @media screen and (max-width: 1100px) {
+    .contact-container {
+      display: flex;
+      flex-direction: column;
+      &-image {
+        min-width: 0px;
+      }
+      .form-container {
+        width: auto;
+        min-width: 0px;
+      }
+    }
   }
 </style>
